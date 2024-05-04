2f079a8b0064fabde9c3f2decf383f57a9cf065a
lcu-haoboli <kevin.li@lincolnuni.ac.nz>
2024-05-04 20:54:54 +1200

    improve users table

--- database/mcrs-local.sql
+++ database/mcrs-local.sql
@@ -70,8 +70,10 @@ CREATE TABLE user_roles (
 );
 
 CREATE TABLE users (
-    user_id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id varchar(36) PRIMARY KEY,
     role_id INT,
+    first_name VARCHAR(250) NOT NULL,
+    last_name VARCHAR(250) NOT NULL,
     email VARCHAR(255) UNIQUE NOT NULL,
     password VARCHAR(255) NOT NULL,
     FOREIGN KEY (role_id) REFERENCES user_roles(role_id)
