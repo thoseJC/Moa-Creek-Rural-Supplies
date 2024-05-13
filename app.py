edef6433e7e02e5f5f36e2d6cd8f5c78972b8afd
MiZhao1159170 <150638016+MiZhao1159170@users.noreply.github.com>
2024-05-13 17:54:31 +1200

    show single product page.

--- app.py
+++ app.py
@@ -159,3 +159,35 @@ def manage_own_profile():
 	return render_template('global/manage_own_profile.html',
 		user=user, 
 		msg_obj=msg_obj)
+
+@app.route('/product/<int:product_id>')
+def show_product(product_id):
+	cursor = getCursor()
+	sql_query = """
+		SELECT 
+			p.product_id,
+			p.name,
+			p.description,
+			p.price,
+			p.pd_image_path,
+			c.name,
+			p.is_active
+		FROM products p 
+		JOIN categories c
+		ON p.category_id = c.category_id
+		WHERE p.product_id = %s;
+	"""
+	cursor.execute(sql_query, (product_id,))
+	fetched_product = cursor.fetchone()
+	product = {
+		"id":fetched_product[0],
+		"name": fetched_product[1],
+		"description": fetched_product[2],
+		"price": fetched_product[3],
+		"image": fetched_product[4],
+		"category": fetched_product[5],
+		"active": fetched_product[6]
+
+	}
+
+	return render_template('product/product_info.html',product = product)
