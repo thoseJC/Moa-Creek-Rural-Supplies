8a61f548aa0cee1a973084da992ff657c5a7ef82
MiZhao1159170 <150638016+MiZhao1159170@users.noreply.github.com>
2024-05-16 19:44:12 +1200

    edit product. delete product.

--- app.py
+++ app.py
@@ -228,7 +228,72 @@ def add_product():
 		return render_template('product/add_product.html', selected_category_id=category_id, categories=categories)
 
 
+@app.route('/product/edit/<int:product_id>', methods=['GET', 'POST'])
+def edit_product(product_id):
+    if request.method == 'POST':
+        # Retrieve form data
+        name = request.form['name']
+        description = request.form['description']
+        price = request.form['price']
+        image_path = request.form['image_path']
+        category_id = request.form['category_id']
 
+        # Update the product in the database
+        cursor = getCursor()
+        sql_query = """
+            UPDATE products 
+            SET name=%s, description=%s, price=%s, pd_image_path=%s, category_id=%s
+            WHERE product_id=%s
+        """
+        cursor.execute(sql_query, (name, description, price, image_path, category_id, product_id))
+
+        # Redirect to the product info page of the updated product
+        return redirect(url_for('show_product', product_id=product_id))
+    else:
+        # Fetch the product details from the database
+        cursor = getCursor()
+        sql_query = """
+            SELECT 
+                p.product_id,
+                p.name,
+                p.description,
+                p.price,
+                p.pd_image_path,
+                c.category_id,
+                p.is_active
+            FROM products p 
+            JOIN categories c
+            ON p.category_id = c.category_id
+            WHERE p.product_id = %s;
+        """
+        cursor.execute(sql_query, (product_id,))
+        fetched_product = cursor.fetchone()
+        product = {
+            "id": fetched_product[0],
+            "name": fetched_product[1],
+            "description": fetched_product[2],
+            "price": fetched_product[3],
+            "image": fetched_product[4],
+            "category_id": fetched_product[5],
+            "active": fetched_product[6]
+        }
+
+        # Retrieve category options from the database
+        sql_query = "SELECT category_id, name FROM categories;"
+        cursor.execute(sql_query)
+        categories = cursor.fetchall()
+
+        return render_template('product/edit_product.html', product=product, categories=categories)
+
+@app.route('/product/delete/<int:product_id>', methods=['POST'])
+def delete_product(product_id):
+    # Delete the product from the database
+    cursor = getCursor()
+    sql_query = "DELETE FROM products WHERE product_id = %s"
+    cursor.execute(sql_query, (product_id,))
+
+    # Redirect to a page after deletion (e.g., product listing page)
+    return redirect(url_for('show_product', product_id=1))
  
 
 @app.route('/register', methods=['GET', 'POST'])
