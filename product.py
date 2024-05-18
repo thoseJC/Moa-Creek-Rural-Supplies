2a7f20dcab1a53a93dae8b9e04f920da240b5078
lcu-haoboli <kevin.li@lincolnuni.ac.nz>
2024-05-18 15:16:34 +1200

    Refactor: extract get product by ID function from app.py file

--- product.py
+++ product.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, flash, redirect, url_for, jsonify,render_template,session
+from app_query import query_product_by_id
 from cursor import getCursor
 
 
@@ -8,3 +9,22 @@ product_page = Blueprint("product_page", __name__, static_folder="static", templ
 @product_page.route("/product-management")
 def get_list_of_product():
 	return render_template("product-management.html")
+
+@product_page.route('/<int:product_id>')
+def show_product(product_id):
+	cursor = getCursor()
+	sql_query = query_product_by_id()
+	cursor.execute(sql_query, (product_id,))
+	fetched_product = cursor.fetchone()
+	product = {
+		"id":fetched_product[0],
+		"name": fetched_product[1],
+		"description": fetched_product[2],
+		"price": fetched_product[3],
+		"image": fetched_product[4],
+		"category": fetched_product[5],
+		"active": fetched_product[6]
+
+	}
+
+	return render_template('product_info.html',product = product)
