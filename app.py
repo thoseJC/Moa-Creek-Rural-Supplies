1c47ab2d7db62230928b02143eecbc85c3af574d
Jason <jason@moustachrepublic.com>
2024-05-19 21:22:43 +1200

    chore: split cart related logic into cart.py

--- app.py
+++ app.py
@@ -10,9 +10,7 @@ from message import message_page
 from login import login_page
 from register import register_page
 from user_profile import profile_page
-
-from cursor import getCursor
-from app_query import get_products_by_ids
+from cart import cart_page
 
 
 app.register_blueprint(manager_page, url_prefix="/manager")
@@ -25,44 +23,12 @@ app.register_blueprint(message_page, url_prefix="/message")
 app.register_blueprint(login_page, url_prefix="/login")
 app.register_blueprint(register_page, url_prefix="/register")
 app.register_blueprint(profile_page, url_prefix="/profile")
+app.register_blueprint(cart_page, url_prefix="/cart")
 
 
 @app.route("/")
 def home():
     return render_template('global/index.html')
-
-@app.route("/cart")
-def cart():
-    user_id = 0
-    if session.get("user_id"):
-        user_id = session.get("user_id")
-    return render_template('global/cart.html', user_id = user_id)
-
-
-@app.route("/get_products", methods=["POST"])
-def get_products():
-    products = []
-    try:
-        product_ids = request.json.get("product_ids", [])
-        cursor = getCursor()
-        sql_query = get_products_by_ids(product_ids)
-        cursor.execute(sql_query)
-        for product in cursor:
-            products.append({
-                "product_id": product[0],
-                "category_id": product[1],
-                "name": product[2],
-                "description": product[3],
-                "price": product[4],
-                "pd_image_path": product[5],
-                "is_active": product[6]
-            })
-        cursor.close()
-        return products
-    except Exception as e:
-        print("@app.route(/get_products): %s", e)
-        return products
-
 
 @app.route('/logout')
 def logout():
