b442bb8a4356f608fba4357d4bc573721c2b8f3b
Jason <jason@moustachrepublic.com>
2024-05-25 22:45:11 +1200

    chore: customer credit temp work

--- customer.py
+++ customer.py
@@ -7,15 +7,18 @@ from customer_query import category_list_query
 
 customer_page = Blueprint("customer", __name__, static_folder="static", template_folder="templates/customer")
 
-@customer_page.route("/dashboard")
-def dashboard():
-  user = {
+def get_user_info():
+  return {
     "user_id": session.get("user_id"),
     "user_role": session.get("user_role"),
     "first_name": session.get("first_name"),
     "last_name": session.get("last_name"),
     "order_count": session.get("order_count")
   }
+
+@customer_page.route("/dashboard")
+def dashboard():
+  user = get_user_info()
   if session.get('logged_in') != True or user["user_role"] != 'customer':
     return redirect(url_for('login_page.login'))
   return render_template("global/account_dashboard.html", user=user)
@@ -28,3 +31,8 @@ def categories():
     categories_list = connection.fetchall()
 
     return render_template("global/categories.html", categories_list=categories_list)
+
+@customer_page.route("/credit")
+def credit():
+  user = get_user_info()
+  return render_template("credit.html", user=user)
