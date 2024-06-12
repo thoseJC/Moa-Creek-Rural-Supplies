257e1d03ab532364ada6a6bffa67d38beb34fc0c
thoseJC <143766236+thoseJC@users.noreply.github.com>
2024-06-12 20:06:17 +1200

    fixed a route which was changed by mistake

--- product.py
+++ product.py
@@ -34,39 +34,34 @@ def get_list_of_product():
 
 @product_page.route('/<int:product_id>')
 def show_product(product_id):
-    try:
-        cursor = getCursor()
+	try:
+		cursor = getCursor()
+		sql_query = query_product_by_id()
+		cursor.execute(sql_query, (product_id,))
+		fetched_product = cursor.fetchone()
+		cursor.execute("select * from categories")
+		categories = cursor.fetchall()
+		product = {
+			"id":fetched_product[0],
+			"name": fetched_product[1],
+			"description": fetched_product[2],
+			"price": fetched_product[3],
+			"discount": fetched_product[4],
+			"discounted_price": fetched_product[5],
+			"image": fetched_product[6],
+			"category": fetched_product[7],
+			"active": fetched_product[8],
+			"quantity": fetched_product[9],
+			"categoryId" : fetched_product[10]
+		}
+		categoryId = product["categoryId"]
+		cursor.execute("select * from products where category_id = %s",(categoryId,))
+		products = cursor.fetchall();
+		return render_template('product_info.html',product = product, products= products, categories =categories, error_msg = None)
+	except Exception as e:
+		print("@app.route('/product'): %s",e)
+		return render_template('product_info.html', error_msg = e)
 
-        sql_query = query_product_by_id()
-        cursor.execute(sql_query, (product_id,))
-        fetched_product = cursor.fetchone()
-
-        product = {
-            "id": fetched_product[0],
-            "name": fetched_product[1],
-            "description": fetched_product[2],
-            "price": fetched_product[3],
-            "discount": fetched_product[4],
-            "discounted_price": fetched_product[5],
-            "image": fetched_product[6],
-            "category": fetched_product[7],
-            "active": fetched_product[8],
-            "quantity": fetched_product[9]
-        }
-
-        category_id = fetched_product[7]
-        sql_query = "SELECT active FROM categories WHERE category_id = %s"
-        cursor.execute(sql_query, (category_id,))
-        category_active = cursor.fetchone()[0]
-
-        if not category_active:
-            return render_template('category_inactive.html', product=product)
-
-        return render_template('product_info.html', product=product)
-
-    except Exception as e:
-        print(f"@app.route('/product'): {e}")
-        return render_template('product_info.html', error_msg=str(e))
 
 
 @product_page.route('/add', methods=['GET', 'POST'])
